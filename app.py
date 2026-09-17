04e0f304213839a46d83200293158d054a0bbeaf
Aldemir <aldemirromario@gmail.com>
2026-09-17 04:24:07 -0300

    Conversão de texto pra número e soma de totais

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import csv
 from pathlib import Path
-#from decimal import Decimal
+from decimal import Decimal
 
 pasta_projeto = Path(__file__).resolve().parent
 caminho_arquivo = pasta_projeto / "dados" / "gastos.csv"
@@ -9,5 +9,14 @@ with open(caminho_arquivo, mode = "r", encoding="utf-8-sig") as arquivo:
     leitor = csv.DictReader(arquivo, delimiter = ";")
     gastos = list(leitor)
 
+total = Decimal("0.00")
+
+
 for gasto in gastos:
-    print(gasto)
+    valor = Decimal(gasto["valor"])
+    total += valor
+
+total_formatado = f"{total:.2f}".replace(".", ",")
+
+print(f"Quantidade de gastos: {len(gastos)}")
+print(f"Total de gastos: R$ {total_formatado}")
