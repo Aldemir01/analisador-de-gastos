6aa24b7a9b0a74a524bf80990ac916f69b28aeb1
Aldemir <aldemirromario@gmail.com>
2026-09-17 04:45:25 -0300

    Separação por Categoria de Gasto

--- app.py
+++ app.py
@@ -10,13 +10,26 @@ with open(caminho_arquivo, mode = "r", encoding="utf-8-sig") as arquivo:
     gastos = list(leitor)
 
 total = Decimal("0.00")
-
+totais_por_categoria = {}
 
 for gasto in gastos:
     valor = Decimal(gasto["valor"])
+    categoria = gasto["categoria"]
+
     total += valor
+
+    if categoria not in totais_por_categoria:
+        totais_por_categoria[categoria] = Decimal("0.00")
+
+    totais_por_categoria[categoria] += valor
 
 total_formatado = f"{total:.2f}".replace(".", ",")
 
 print(f"Quantidade de gastos: {len(gastos)}")
 print(f"Total de gastos: R$ {total_formatado}")
+
+print("\nTotais por categoria:")
+
+for categoria, subtotal in totais_por_categoria.items():
+    subtotal_formatado = f"{subtotal:.2f}".replace(".", ",")
+    print(f"- {categoria}: R$ {subtotal_formatado}")
